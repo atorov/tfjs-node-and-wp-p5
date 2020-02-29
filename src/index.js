ce65e774bca8626e93488429c36c77d55262683a
atorov <veselin.lkp@gmail.com>
2020-02-29 23:01:52 +0200

    adding some basic examples

--- src/index.js
+++ src/index.js
@@ -22,3 +22,41 @@ model.fit(xs, ys, { epochs: 10 }).then(() => {
 })
 
 console.log('::: tfjs backend:', tf.getBackend())
+
+tf.tidy(() => {
+    // scalar
+    tf.tensor([87], []).print()
+    tf.scalar(87).print()
+
+
+    // 1D tensor
+    tf.tensor([1, 2, 3], [3]).print()
+    tf.tensor1d([1, 2, 3]).print()
+
+    // 2D tensor
+    tf.tensor([1, 2, 3, 4, 5, 6], [2, 3]).print()
+    tf.tensor2d([[1, 2, 3], [4, 5, 6]]).print()
+
+    // 3D tensor
+    tf.tensor([1, 2, 3, 4, 5, 6, 7, 8], [2, 2, 2]).print()
+    tf.tensor3d([[[1, 2], [3, 4]], [[5, 6], [7, 8]]]).print()
+
+    const a = tf.tensor1d([1, 2, 3, 4])
+    const b = tf.tensor1d([2, 2, 2, 2])
+    a.add(b).print()
+    a.print()
+})
+
+setTimeout(() => {
+    console.log('::: tensors:', tf.memory().numTensors)
+
+    tf.tidy(() => {
+        for (let i = 0; i < 1000; i += 3) {
+            const ts = tf.scalar(2)
+            const t1 = tf.tensor1d([i, i + 1, i + 2])
+            t1.mul(ts)
+        }
+    })
+
+    console.log('::: tensors:', tf.memory().numTensors)
+}, 2550)
